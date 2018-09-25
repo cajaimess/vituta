6a739ae8340742ac9b484fc99d2ed2aeb1b25e93
USUARIO <USUARIO@USUARIO-PC>
2018-09-25 04:44:43 -0500

    Cambio base de datos

--- persistencia/conexion/Conexion.php
+++ persistencia/conexion/Conexion.php
@@ -5,7 +5,7 @@ use PDO;
 
 class Conexion {
     public static function conectar(){
-        $cnn= new PDO('pgsql: host=localhost; dbname=vituta','postgres', 'postgres');
+        $cnn= new PDO('pgsql: host=ec2-107-21-233-72.compute-1.amazonaws.com; dbname=dnufcsudkbr4m','oubnjifbuqmcgn', 'f6ac0eb045514a9a2f1a823b683999430bd2ee0c20ccaa74e70d14f1cc8b2b0a');
         $cnn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $cnn;
     }
